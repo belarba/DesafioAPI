2a2e5fff28ebcb0235b917839fb93b070f0c8b3f
Daniel Belarmino <belarba@gmail.com>
2019-06-01 19:25:45 -0300

    passing event

--- app/controllers/desafios_controller.rb
+++ app/controllers/desafios_controller.rb
@@ -1,4 +1,10 @@
 class DesafiosController < ApplicationController
   def test
+
   end
+
+  def input_insert
+    raise
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'test', to: 'desafios#test'
+  post '/input_insert', to: 'desafios#input_insert'
 
   namespace 'api' do
     namespace 'v1' do
